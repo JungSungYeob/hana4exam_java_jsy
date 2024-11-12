96bc5331b339de7005ad89a4fd25fa3acec987d3
Sung Yeob Jung <dev.sungyeob@gmail.com>
2024-11-12 15:52:46 +0900

    hotfix code

--- src/hanabank/FreeAccount.java
+++ src/hanabank/FreeAccount.java
@@ -18,9 +18,9 @@ public class FreeAccount extends Account {
 
 	@Override
 	public void transferMoney(Map<Integer, Account> accountList) {
+		Account account = super.selectAccount(accountList);
 		while (true) {
 			try {
-				Account account = super.selectAccount(accountList);
 				System.out.printf("\t%s 통장에 보낼 금액은? ", account.name);
 				String money = Tool.scanNext();
 				Tool.isEnough(money, super.balance);
@@ -28,11 +28,14 @@ public class FreeAccount extends Account {
 				Tool.printGreen("\t%s 통장의 잔액은 %s원입니다.".formatted(super.name, Tool.decimalFormat(super.balance)));
 				break;
 			} catch (NotEnoughMoneyException | InvalidInputException | NumberFormatException e) {
+				if (e instanceof NotEnoughMoneyException) {
+					System.out.println("hello");
+					continue;
+				}
 				if (e instanceof NumberFormatException) {
 					Tool.printDanger("\t계좌 번호는 숫자로만 이루어져있습니다. 정확한 입력을 해주십시오.");
 				} else {
 					Tool.printDanger(e.getMessage());
-					continue;
 				}
 			}
 		}
